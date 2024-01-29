b69ffdd9ad6bd11bacf5e5120e3cd9e76a9ebc82
Fredrik <fredrik.gangso@gmail.com>
2024-01-29 14:25:50 +0100

    Refactor code to improve performance and readability

--- src/pysmartdok.py
+++ src/pysmartdok.py
@@ -1,73 +1,58 @@
-from multiprocessing import Pool
-import datetime
-import sys
 import logging
 import json
 
-from bs4 import BeautifulSoup as bs
 import requests
+from bs4 import BeautifulSoup as bs
 
-# TODO: REMOVE AFTER DEVELOPMENT
-## FIXME: REMOVE ALL BELOW CODE UP TO NEXT FIXME
-import os
-from pathlib import Path
-import dotenv
-
-script_dir = Path(os.path.dirname(os.path.realpath(__file__)))
-
-envfile = Path(script_dir.parent, '.env')
-if not envfile.exists():
-    print(f'envfile {envfile} not found')
-    exit(1)
-
-dotenv.load_dotenv(dotenv_path=envfile)
-
-envvars = {}
-
-# envvars that are required to run
-required_envvars = ['SMARTDOK_USERNAME', 'SMARTDOK_PASSWORD', 'SMARTDOK_INTEGRATION_TOKEN']
-for required_envvar in required_envvars:
-    if required_envvar not in os.environ:
-        print('Error: Required envvar not in environment: ' + required_envvar)
-        exit(1)
-
-    envvars[required_envvar] = os.environ[required_envvar]
-
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-logging.debug(f'envvars: {envvars}')
-## FIXME: REMOVE ALL ABOVE CODE UP TO NEXT FIXME
-# TODO: REMEMBER TO REMOVE AFTER DEVELOPMENT
+import utils
 
 class Client:
-    def __init__(self, username: str = None, password: str = None, integration_token: str = None, user_agent: str = 'intrix-pysmartdok(post@intrix.no)'):
-        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-        
-        if None in (username, password, integration_token):
-            raise ValueError("username, password, and integration token must be provided.")
+    def __init__(self, username: str = None, password: str = None, user_agent: str = 'intrix-pysmartdok(post@intrix.no)'):
+        """
+        initializes an instance of the `pysmartdok` class.
 
-        self.session = requests.Session()
-        # TODO: ADD CONMMENT ABOUT WHY WE USE THIS USER AGENT
+        args:
+            username (str, required): the username for SmartDok login. defaults to None.
+            password (str, required): the password for SmartDok login. defaults to None.
+            user_agent (str, optional): the user agent to be used for the HTTP requests. defaults to 'intrix-pysmartdok(post@intrix.no)'.
+        """
+        logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] - %(asctime)s - %(message)s')
+        utils.verify_init_params(username, password, user_agent)
+        
+        # we use this user agent because SmartDok blocks the default Python user agent.
+        # it's necessary to employ a user agent that SmartDok does not block.
+        # the 'X-CSRF' header is required by SmartDok to prevent CSRF attacks.
+        # we initialize it to be empty as its specific value is unknown; it only needs to be present in the headers.
         headers = {
             'User-Agent': f'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 [{user_agent}]',
             'X-CSRF': '',
         }
+
+        self.session = requests.Session()
         self.session.headers.update(headers)
-        logging.debug(f'current headers for session: \n {self.session.headers}')
 
         self.base_url = 'https://web.smartdok.no'
         self.web_api_url = f'{self.base_url}/smartapi'
 
-        login = self.inisiat_smartdok_web_api(username, password)
+        if self.smartdok_login(username, password) == 200: logging.debug('smartdok_web_login - success')
 
-    def inisiat_smartdok_web_api(self, username: str, password: str):
-        if self.smartdok_web_login(username, password) == 200: logging.debug('smartdok_web_login - success')
-        self.inisiat_api()
+    def smartdok_login(self, username: str = None, password: str = None) -> int:
+        """
+        logs into the SmartDok website using the provided username and password.
 
-    def smartdok_web_login(self, username: str, password: str) -> requests.status_codes:
-        # TODO: ADD CONMMENT ABOUT WHY WE HAVE STRUCTURED THE FORM DATA LIKE THIS
+        args:
+            username (str): the username for logging into SmartDok.
+            password (str): the password for logging into SmartDok.
+
+        returns:
+            int: the status code of the login request.
+
+        raises:
+            Exception: if the login request fails or the login is unsuccessful.
+        """
+        # we have structured the form data like this because we need to retrieve the values of the hidden inputs.
+        # this approach helps avoid the need to write the long key name every time we interact with the hidden input values.
         raw_form_data = {
-            'eventtarget':          {'key': '__EVENTTARGET', 'value': ''},
-            'eventargument':        {'key': '__EVENTARGUMENT', 'value': ''},
             'viewstate':            {'key': '__VIEWSTATE', 'value': ''},
             'viewstategenerator':   {'key': '__VIEWSTATEGENERATOR', 'value': ''},
             'eventvalidation':      {'key': '__EVENTVALIDATION', 'value': ''},
@@ -76,101 +61,100 @@ class Client:
             'password':             {'key': 'SmartDokLoginView$LoginSmartDok$Password', 'value': password},
             'loginbutton':          {'key': 'SmartDokLoginView$LoginSmartDok$LoginButton', 'value': ''}
         }
-        logging.debug(f'smartdok_web_login - form_data: {raw_form_data}')
 
         login_page = self.session.get(self.base_url)
         soup = bs(login_page.text, 'html.parser')
 
         for meta_key, inner_dict in raw_form_data.items():
-            # TODO: ADD CONMMENT ABOUT WHY WE SKIP USERNAME AND PASSWORD
+            # we skip username and password because we already have them in the raw_form_data dict.
             if meta_key == 'username' or meta_key == 'password':
                 continue
-            inner_dict['value'] = self.soup_find_input_value(soup, inner_dict['key'])
+            inner_dict['value'] = utils.soup_find_input_value(soup, inner_dict['key'])
 
-        form_data = self.dict_with_dict_to_dict(raw_form_data)
+        form_data = utils.dict_with_dict_to_dict(raw_form_data)
 
-        response = self.session.post(f'{self.base_url}/index.aspx', data=form_data)
+        login = self.session.post(f'{self.base_url}/index.aspx', data=form_data)
+       
+        utils.verify_login(login)
 
-        if response.status_code != 200:
-            raise Exception(f'Login failed {response.status_code}')
-        
-        response_soup = bs(response.text, 'html.parser')
-
-        if response_soup.find('span', attrs={'id': 'LabelCompanyName'}).text == None:
-            raise Exception('Login failed - no company name found')
-
-        logging.debug(f'smartdok_web_login - response_soup: {response_soup}')
-
-        return response.status_code
+        return login.status_code
     
+    def get_single_record(self, record_id: str = None, record_type: str = None) -> dict:
+            """
+            retrieves a single record from the SmartDok API.
 
+            args:
+                record_id (str): the ID of the record to retrieve.
+                record_type (str): the type of the record to retrieve.
+
+            returns:
+                dict: the processed data of the retrieved record.
+
+            raises:
+                Exception: if the request to the API fails.
+                ValueError: if the record_type is unknown.
+            """
+            utils.verify_record_type(record_type)
+            
+            params = {
+                'id': record_id,
+                'getAccessRights': 'true'
+            }
+
+            request = self.session.get(f'{self.web_api_url}/{record_type}/report', params=params)
+
+            if request.status_code != 200:
+                raise Exception(f'request status code {request.status_code} != 200')
+
+            respone_data = json.loads(request.text)
+
+            if record_type == 'qd':
+                processed_data = utils.convert_smartdok_qd_record_to_dict(respone_data)
+            elif record_type == 'rue':
+                processed_data = utils.convert_smartdok_rue_record_to_dict(respone_data)
+            else:
+                raise ValueError(f'Unknown record_type: {record_type}')
+
+            return processed_data
+
+    def get_all_records(self, record_type: str = None, days_back: int = 0) -> list:
+        """
+        retrieves all records of a specified type within a given time range.
+
+        args:
+            record_type (str): the type of records to retrieve.
+            days_back (int): the number of days back from today to retrieve records.
+
+        returns:
+            list: a list of records.
+
+        raises:
+            ValueError: if the record_type is not valid.
+        """
+        utils.verify_record_type(record_type)
         
-    def inisiat_api(self):
-        payload = {
-            'Submitter': '',
-            'DepartmentId': '',
-            'FromDate': '',
-            'ToDate': '',
-            'IncludeDeactivated': 'false',
-            'HideStatus[]': '3',
-            'take': '20',
-            'skip': '0',
-            'page': '1',
-            'pageSize': '20'
+        records = []
+
+        if days_back > 0:
+            today_date, days_back_date = utils.convert_date_to_smartdok_date_format(days_back)
+        else:
+            today_date = ''
+            days_back_date = ''
+
+        params = {
+            'FromDate': days_back_date,
+            'ToDate': today_date,
+            'take': '',
         }
 
-
-        test = self.session.get(f'{self.web_api_url}/qd/overview' , params=payload)
-
-        # make text to dict
-        test = json.loads(test.text)
-
-        print(test)
-
-    @staticmethod
-    def soup_find_input_value(soup: bs, input_name: str) -> str:
-        """
-        soup_find_input is a static method that is used to find the value of an input element in a BeautifulSoup object.
-
-        Parameters:
-        - soup (BeautifulSoup): The BeautifulSoup object to search in.
-        - input_name (str): The name attribute of the input element to find.
-
-        Returns:
-        - str: The value of the found input element.
-        """
-        input = soup.find('input', attrs={'name': input_name})
-
-        if input == None:
-            raise Exception(f'no input found with name {input_name}')
-
-        input_value = input['value']
+        request_all_records = self.session.get(f'{self.web_api_url}/{record_type}/overview', params=params)
         
-        logging.debug(f'soup_find_input - name={input_name}, value={input_value}')
+        request_all_records_data_raw = json.loads(request_all_records.text)
+        request_all_records_data = request_all_records_data_raw['data']
 
-        return input_value
-    
-    @staticmethod
-    def dict_with_dict_to_dict(input_dict: dict) -> dict:
-        """
-        Converts a dictionary with nested dictionaries to a flat dictionary.
-
-        Args:
-            input_dict (dict): The input dictionary with nested dictionaries.
-
-        Returns:
-            dict: The resulting flat dictionary.
-        """
-        output_dict = {}
-
-        for key in input_dict:
-            return_key = input_dict[key]['key']
-            output_dict[return_key]= input_dict[key]['value']
+        for record in request_all_records_data:
+            record_raw = Client.get_single_record(record['Id'], record_type)
             
-        logging.debug(f'dict_with_dict_to_dict - from {input_dict} \n to {output_dict}')
-            
-        return output_dict
+            records.append(record_raw)
 
-
-if __name__ == "__main__":
-    client = Client(envvars['SMARTDOK_USERNAME'], envvars['SMARTDOK_PASSWORD'], envvars['SMARTDOK_INTEGRATION_TOKEN'])
+        return records
