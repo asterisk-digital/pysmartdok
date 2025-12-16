f5ebc1ef874eb3c90df65cde405ba888a6d19145
David Skoland <davidskoland@gmail.com>
2025-12-16 14:00:48 +0100

    fix tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,7 +3,26 @@ import pysmartdok
 def test_trivial():
     assert True
 
-def test_api_client():
-    api_client = pysmartdok.ApiClient(api_token='')
+def test_api_client_without_token():
+    api_client = None
 
-    assert api_client
+    try:
+        api_client = pysmartdok.ApiClient(api_token='')
+    except ValueError as e:
+        assert True
+    else:
+        assert False
+
+    assert api_client is None
+
+def test_api_client_with_token():
+    api_client = None
+
+    try:
+        api_client = pysmartdok.ApiClient(api_token='test')
+    except pysmartdok.api_client.SmartDokApiError as e:
+        assert True
+    else:
+        assert False
+
+    assert api_client is None
