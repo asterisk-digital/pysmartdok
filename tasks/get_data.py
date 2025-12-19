617cc5249539a3f6fdced044557e2cee98efa1c0
David Skoland <davidskoland@gmail.com>
2025-12-19 15:44:00 +0100

    simplify get_data

--- tasks/get_data.py
+++ tasks/get_data.py
@@ -1,26 +1,17 @@
 import os
-import sys
-from pathlib import Path
 
 import dotenv
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
-
-sys.path.append(script_dir + "../src")
-
-from pysmartdok import apiclient
+import pysmartdok
 
 
 def main():
-    envfile = script_dir + "/../.env-prod"
-    if not Path(envfile).exists():
-        raise FileNotFoundError(f"Could not find envfile: {envfile}")
+    dotenv.load_dotenv()
+    client = pysmartdok.ApiClient(api_token=os.getenv("SMARTDOK_API_TOKEN"))
 
-    dotenv.load_dotenv(envfile)
-    client = apiclient.ApiClient(api_token=os.getenv("SMARTDOK_API_TOKEN"))
-
-    qds = client.get_qd()
-    print(qds)
+    #qds = client.get_qd()
+    data = client.get_projects()
+    print(data)
 
 
 if __name__ == '__main__':
