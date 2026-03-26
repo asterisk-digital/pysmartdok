24d77c64e994efcdd84823633107b35a7c988de1
David Skoland <davidskoland@gmail.com>
2026-03-26 16:09:20 +0100

    add get_users

---
+++ tasks/get_users.py
@@ -0,0 +1,17 @@
+import os
+
+import dotenv
+
+import pysmartdok
+
+
+def main():
+    dotenv.load_dotenv()
+    client = pysmartdok.ApiClient(api_token=os.getenv("SMARTDOK_API_KEY"))
+
+    data = client.users.get_users(include_verified=True)
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
